a1c7227a77da364777ddbf6611b763acc8007b17
sharma39vishal <sharma39vishal@gmail.com>
2023-10-07 09:52:21 +0530

    with preview

--- src/components/SingleFiles.jsx
+++ src/components/SingleFiles.jsx
@@ -10,10 +10,20 @@ export default function SingleFiles({ url }) {
   const isPDF = url.endsWith('.pdf');
   const isImage = /\.(jpeg|jpg|png|gif|bmp)$/i.test(url);
   const isVideo = /\.(mp4|avi|mov|wmv)$/i.test(url);
+  const supportedFileExtensions = /\\(jpg|jpeg|png|gif|bmp|svg|mp4|ogg|webm|pdf)\\?/i.test(url);
+  // const supportedFileExtensions = /\.(jpg|jpeg|png|gif|bmp|svg|mp4|ogg|webm|pdf)$/i.test(url);
+  // const supportedFileExtensions = /https:\/\/firebasestorage\.googleapis\.com\/v0\/b\/vishal-6ccf0\.appspot\.com\/o\/files%2F[^?]+(\.jpg|\.jpeg|\.png|\.gif|\.bmp|\.svg|\.mp4|\.ogg|\.webm|\.pdf)\?alt=media&token=[^&]+$/
+  const keywords = ['jpg','jpeg','png','gif','bmp','svg','mp4','ogg','webm','pdf'];
+  const pattern = new RegExp(keywords.join('|'), 'i'); 
   
-  if(isWebPage){
-    return "Valid";
+  if(!pattern.test(url)){
+    return (
+      <div key={url} className="my-4 mx-4">
+        <a href={url} style={{ color: "white" }}><p>{url}</p></a>
+      </div>
+    );
   }
+
   if (isWebPage) {
     return (
       <div className='single-file-box'>
@@ -24,43 +34,7 @@ export default function SingleFiles({ url }) {
         ></iframe>
       </div>
     );
-  } else if (isPDF) {
-    return (
-      <div className='single-file-box'>
-        <object
-          data={url}
-          type="application/pdf"
-          onError={onError}
-        >
-          PDF Viewer is not supported in this browser. You can{' '}
-          <a href={url} download>download the PDF</a> instead.
-        </object>
-      </div>
-    );
-  } else if (isImage) {
-    return (
-      <div className='single-file-box'>
-        <img
-          src={url}
-          alt="Image Preview"
-          style={{ maxWidth: '100%', height: 'auto' }}
-          onError={onError}
-        />
-      </div>
-    );
-  } else if (isVideo) {
-    return (
-      <div className='single-file-box'>
-        <video
-          controls
-          onError={onError}
-        >
-          <source src={url} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-    );
-  } else {
+  }else {
     return (
       <div key={url} className="my-4 mx-4">
         <a href={url} style={{ color: "white" }}><p>{url}</p></a>
